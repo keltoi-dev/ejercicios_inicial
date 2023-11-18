ab1666166a8446454b966140a5cf320e4446d015
keltoi-dev <gdfraga@gmail.com>
2023-11-17 21:30:26 -0300

    Primer commit

---
+++ m1_u4_ej4 - copia.py
@@ -0,0 +1,9 @@
+# Ejercicio 4 - Unidad 4
+# German Fraga
+
+"""
+Crear una función lambda que tome como parámetro una frase y la escriba al revés.
+"""
+
+frase = lambda oracion: oracion[::-1]
+print(frase("La casa es roja"))
