e3d645f38dbffea458ac67f953c86114a286b7c0
keltoi-dev <gdfraga@gmail.com>
2023-11-19 16:53:27 -0300

    Mas ejercicios

--- m1_u4_ej7.py
+++ m1_u4_ej7.py
@@ -2,9 +2,37 @@
 # German Fraga
 
 """
-Cree una función que tome la siguiente lista y reordene de menor a mayor sus componentes:
-[3, 44, 21, 78, 5, 56, 9]
+isinstance(x, list) permite consultar si el elementos x es del tipo lista.A partir de la siguiente función recursiva que permite
+permite consultar si el elementos x es del tipo lista. 
+A partir de la siguiente función recursiva que permite recorrer los niveles de una lista:
+recorrer los niveles de una lista: 
+
+lista = ["elemento1n1", "elemento2n1", "elemento3n1",["elemento1n2", "elemento2n2", "elemento3n2",["elemento1n3", "elemento2n3", "elemento3n3"]]] 
+def recorre_lista(item):     
+    for x in item:         
+        if isinstance(x, list):             
+            recorrer_lista(x)        
+        else:             
+            print(x)  
+recorrer_lista(lista) 
+
+Optimice el código de forma que el programa considere:
+ Un valor de lista por defecto
+ Permita tomar un segundo parámetro que lleve un encuentra (en qué grado del anidamiento)
+ Permita tomar un valor por defecto de cero para el parámetro anterior.
+ Presente la salida según el siguiente formato:
+
+elemento1n1 
+elemento2n1 
+elemento3n1     
+   elemento1n2     
+   elemento2n2     
+   elemento3n2         
+      elemento1n3         
+      elemento2n3         
+      elemento3n3 
 """
+
 contador = 0
 lista = ["elemento1n1", "elemento2n1", "elemento3n1", 
         ["elemento1n2", "elemento2n2", "elemento3n2", 
